534c5ddeddb23bc6d9afae20dd6f0cea16dd8f96
Derrick <derrick.xiang@gmail.com>
2019-07-06 14:51:31 +0800

    first commit

---
+++ config/database.php
@@ -0,0 +1,25 @@
+<?php
+class Database{
+
+    private $host = "localhost";
+    private $db_name = "test";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+
+        $this->conn = null;
+
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $excpetion) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+
+?>
